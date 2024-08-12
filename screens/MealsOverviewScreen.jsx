c568b4ec4d15cfb47bd0e9b39937ddfd75752d79
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-08-12 05:17:15 +0200

    create meals screen and get route params

---
+++ screens/MealsOverviewScreen.jsx
@@ -0,0 +1,18 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+
+export default function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+  return (
+    <View style={styles.container}>
+      <Text>Meals Overview Screen - {catId}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
