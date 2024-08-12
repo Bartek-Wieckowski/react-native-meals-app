0266e1e3522184a49587bd5671234ebbf51d6ba0
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-08-12 06:00:32 +0200

    add more styling displayed meals

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,11 +1,29 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 export default function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
     </View>
   );
 }
